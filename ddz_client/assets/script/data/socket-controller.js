e3dfc3139ff2a478cca2ad3b4b33ec74ccccffbf
wendings <152253135@qq.com>
2018-07-16 18:41:17 +0800

    test

--- ddz_client/assets/script/data/socket-controller.js
+++ ddz_client/assets/script/data/socket-controller.js
@@ -1,11 +1,22 @@
 const socket = function () {
     let that = {};
+    let _callBackMap = {};
+    let _callBackMapIndex = 0;
+
     let _socket = io(defines.serverUrl);
     _socket.on('notify',(data)=>{
         console.log('notity = '+JSON.stringify(data));
+        _callBackMapIndex = data.callBackIndex;
+        if(_callBackMap.hasOwnProperty(_callBackMapIndex)){
+            let cb = _callBackMap[_callBackMapIndex];
+            if(data.data.err){
+                cb(data,data.err);
+            }else{
+                cb(null,data.data)
+            }
+        }
     });
-    _socket.on('welcome',(data)=>{
-        console.log('U connection server,listen data = ',JSON.stringify(data));
-    });
+
+
 }
 export default socket;
