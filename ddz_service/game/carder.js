9cd90024f38d80b5431cd8c104505221b2302791
wendings <152253135@qq.com>
2018-08-18 18:28:44 +0800

    2018.8.18.8

--- ddz_service/game/carder.js
+++ ddz_service/game/carder.js
@@ -679,7 +679,7 @@ module.exports = function () {
     // 获得3带几的数组
     const getThreeWithNumCardsList = function (num,cardsA,cardsB){
         let valueA = getRepeatValue(3,cardsA); // 获得重复的牌
-        let list = getRepeatCardsList(3,cardsB);  // 获得重复牌数组
+        let list = getRepeatCardsList(3,cardsB);  // 获得重复牌数组 这里是3带
         let cardList = [];
         for(let i = 0;i<list.length;i++){
             if(list[i][0].value > valueA){
@@ -697,9 +697,184 @@ module.exports = function () {
                 oneCard = oneList[i];
             }
         }
+        // cardList中存储比cardsA 大的牌 ，把带的单牌 oneCard 放入每个cardlist[i]中
+        for(let i = 0;i <cardList.length;i++){
+            let l = cardList[i];
+            if(oneCard !== undefined){
+                for(let j = 0;j <oneCard.length;j++){
+                    l.push(oneCard[j]);
+                }
+            }
+        }
 
+        // 将王炸添加到cardA大的数组中
+        let kingBoom = getKingBoom(cardsB);
+        if(kingBoom !== false){
+            cardList.push(kingBoom);
+        }
+        // 将普通炸弹添加到cardsA大的数组中
+        let fourBoomList = getFourBoom(cardsB);
+        if(fourBoomList !== false){
+            for(let i = 0;i<fourBoomList.length;i++){
+                cardList.push(fourBoomList[i]);
+            }
+        }
+
+        return cardList;
+
+    };
+    // 提示 3带1
+    that.tipsThreeWithOne = function(cardsA,cardsB){
+        // 3,3,3,4
+        return getThreeWithNumCardsList(1,cardsA,cardsB);
+    };
+
+    // 提示 3带2
+    that.tipsThreeWithTwo = function (cardsA, cardsB) {
+        console.log('3带2提示');
+        return getThreeWithNumCardsList(2,cardsA,cardsB);
+    };
+
+    // 获得飞机中牌的最小值
+    const getPlaneMinValue = function (cardsA) {
+        let map = {}; //333,44
+        for(let i = 0;i<cardsA.length;i++){
+            if(map.hasOwnProperty(cardsA[i].value)){
+                map[cardsA[i].value].push(cardsA[i]);
+            }else{
+                map[cardsA[i].value] = [cardsA[i]];
+            }
+        }
+        // {
+        //     '3':[card,card,card],
+        //     '4':[card,card,card]
+        // }
+        let minNum = 100;
+        for(let i in map){
+            if(Number(i) < minNum){
+                // 3 < 100
+                minNum = Number(i);
+            }
+        }
+        return minNum;
+    };
+    // 获得飞机起始
+    const getPlaneWithStart = function (num,cardsB) {
+        let list = getRepeatCardsList(3,cardsB); // 获得牌组中3张重复的牌 并存入数组
+        // 建立map 并哈希 map[index] = list[i]
+        let map = {};
+        for(let i = 0; i<list.length;i++){
+            if(map.hasOwnProperty(list[i][0].value)){
+
+            }else {
+                map[list[i][0].value] = list[i];
+            }
+        }
+        //排序： Object.keys(map).sort()
+        let keys = Object.keys(map);
+        keys.sort(a,b=>{
+            return Number(a) - Number(b);
+        });
+        // 如果是相邻的牌加入临时数组
+        let tempCardsList = [];
+        for(let i = 0;i<(keys.length-1);i++){
+            if(Math.abs(Number(keys[i]) - Number(keys[i]+1)) === 1){
+                let l = [];
+                for(let j = 0;j<map[keys[i]].length;j++){
+                    l.push(map[keys[i]][j]); // {card} 最底层 card 才是对象{}
+                    l.push(map[keys[i+1]][j]);
+                }
+                tempCardsList.push(l);
+            }
+        }
+        // 大于num 放入cardsList数组 返回
+        let cardsList = [];
+        for(let i = 0;i<tempCardsList.length;i++){
+            let valueB = getPlaneMinValue(tempCardsList[i]);
+            if(valueB > num){
+                cardsList.push(tempCardsList[i]);
+            }
+        }
+        return cardsList;
+    };
+    // 提示飞机
+    that.tipsPlane = function (cardsA,cardsB) {
+        console.log('提示飞机');
+        let valueA = getPlaneMinValue(cardsA); //获得飞机中的最小值 ps：333,444 中的 3
+        let cardsList = getCardListWithStart(valueA,cardsB); //获得比最小值大的 牌组
+
+        // 如果有王炸加入王炸
+        let kingBoom = getKingBoom(cardsB);
+        if(kingBoom !== false){
+            cardsList.push(kingBoom);
+        }
+        // 如果有4张牌的普通炸弹放入牌组cardsList
+        let fourBoomList = getFourBoom(cardsB);
+        if(fourBoomList !== false){
+            for(let i = 0;i<fourBoomList.length;i++){
+                cardsList.push(fourBoomList[i]);
+            }
+        }
+        return cardsList;
+    };
+
+    // 提示飞机带单翅膀
+    that.tipsPlaneWithOne = function (cardA,cardB) {
+        let valueA = getPlaneMinValue(cardA); // 获得飞机的最小牌值
+        let cardsList = getPlaneWithStart(valueA,cardB); // 获得比valueA 大的飞机列表
+        let oneCard = getRepeatCardsList(1,cardB); //获得 重复1次的牌列表 ps:牌组中的单牌
+        // 排序
+        oneCard.sort((a,b)=>{
+           return a[0].value - b[0].value;
+        });
+        // 添加翅膀处理
+        if(oneCard.length >=2 ){
+            for(let i = 0;i<cardsList.length;i++){
+                let cards = cardsList[i];
+                for(let j = 0;j<2;j++){
+                    cards.push(oneCard[j][0]);
+                }
+            }
+        }
+        return cardsList;
+    };
+
+    // 提示飞机带双翅膀
+    that.tipsPlaneWithTwo = function(cardA,cardsB){
+        let valueA = getPlaneMinValue(cardsA);
+        let cardsList = getPlaneWithStart(valueA,cardsB);
+        let twoCard= getRepeatCardsList(2,cardsB);
+        //排序
+        twoCard.sort((a,b)=>{
+            return a[0].value - b[0].value;
+        });
+        // 翅膀处理
+        if(twoCard.length >= 2){
+            for(let i = 0;i<cardsList.length;i++){ //循环飞机
+                let cards = cardsList[i];
+                for(let j = 0;j<2;j++){//循环翅膀数组
+                    for(let h = 0;h<twoCard[j].length;h++){//循环单个翅膀数组中的卡牌
+                        cards.push(twoCard[j][h]);
+                    }
+                }
+            }
+        }
+
+        // 王炸添加
+        let kingBoom = getKingBoom(cardsB);
+        if(kingBoom !== false){
+            cardsList.push(kingBoom);
+        }
+        // 炸弹添加
+        let fourBoomList = getKingBoom(cardsB);
+        if(fourBoomList !== false){
+            for(let i = 0;i<fourBoomList.length;i++){
+                cardsList.push(fourBoomList[i]);
+            }
+        }
+
+        return cardsList;
     }
-
 
     // 单牌提示方法，从某个值开始，获取剩下的牌的列表的组合
     const getCardListWithStart = function (start,cards) {
@@ -737,6 +912,191 @@ module.exports = function () {
             }
         }
         return map;
+    };
+
+    // 获得顺子的最小牌
+    const getScrollMinNum = function (cardlist) {
+        let minNum = 100;
+        for(let i = 0;i<cardlist.length;i++){
+            if(cardlist[i].value > minNum){
+                minNum = cardlist[i].value;
+            }
+        }
+        return minNum;
+    }
+
+    // 获得顺子的卡牌列表
+    const getScrollCardsList = function (length,cards) {
+        let cardList = [];
+        let map = {};
+        // 哈希
+        for(let i = 0;i<cards;i++){
+            if(!map.hasOwnProperty(cards[i].value)){
+                cardList.push(cards[i]);
+                map[cards[i].value] = true;
+            }
+        }
+        // 排序
+        cardList.sort((a,b)=>{
+            return a.value - b.value;
+        });
+
+        let cardsList = [];
+        for(let i = 0;i<(cardList.length - length);i++){
+            let list = [];
+            for(let j = i;j< i+length ; j++){
+                list.push(cardList[j]);
+            }
+            cardList.push(list);
+        }
+
+        let endList= [];
+        for(let i = 0;i<cardsList.length; i++){
+            let flag = true;
+            for( let j = 0;j<(cardsList[i].length -1);j++){
+                if(Math.abs(cardsList[i][j].value - cardsList[i][j+1].value) !== 1){
+                    flag = false;
+                }
+            }
+
+            if(flag === true){
+                endList.push(cardsList[i]);
+            }
+        }
+        return endList;
+    };
+    
+    // 提示对子
+    that.tipsScroll = function (cardsA,cardsB) {
+        let valueA = getScrollMinNum(cardsA);
+        let list = getScrollCardsList(cardsA.length,cardsB);
+
+        let cardsList = [];
+        for(let i = 0;i<list.length;i++){
+            let valueB = getScrollMinNum(list[i]);
+            if(valueB > valueA){
+                cardsList.push(list[i]);
+            }
+        }
+        let kingBoom = getKingBoom(cardsB);
+        if(kingBoom !== false){
+            cardsList.push(kingBoom);
+        }
+        let fourBoomList = getFourBoom(cardsB);
+        if(fourBoomList !== false){
+            for(let i = 0;i<fourBoomList.length;i++){
+                cardsList.push(fourBoomList[i]);
+            }
+        }
+        return cardsList;
+    };
+    // 获取连队中最小牌值
+    const getDoubleScorllMinValue = function (cardList) {
+        //33,44,55
+        // [[3,3],[[4,4]],[5,5]]
+        cardList.sort((a,b)=>{
+            return a.value - b.value;
+        });
+        return cardList[0].value;
+    };
+    // 提示连队
+    that.tipsDoubleScroll = function (cardsA,cardsB) {
+        //[[3,3],[4,4],[5,5]]
+        //[3,3,4,4,5,5];
+
+        //3,3,3,4,4,4,5,5,5
+        console.log('cards a = ' + JSON.stringify(cardsA));
+        let valueA = getDoubleScorllMinValue(cardsA);
+        console.log('tips double scroll = ' + valueA);
+        // let list = getRepeatCardsList(2, cardsB);
+        let map = {};
+        for (let i = 0; i < cardsB.length; i++) {
+            let key = -1;
+            if (cardsB[i].king === undefined){
+                key = cardsB[i].value;
+            }else {
+                key = cardsB[i].king;
+            }
+
+
+            if (map.hasOwnProperty(key)) {
+                map[key].push(cardsB[i]);
+            } else {
+                map[key] = [cardsB[i]];
+            }
+        }
+        console.log('map  = ' + JSON.stringify(map));
+        // {
+        //     '3': [card, card,card],
+        //     '4': [card, card, card],
+        //     '5': [card, card, card]
+        // }
+        var list = [];
+        for (let i in map) {
+            if (map[i].length >= 2) {
+                // list.push(map[i].substring(0, 2));
+                let l = [];
+                for (let j = 0; j < 2; j++) {
+                    l.push(map[i][j]);
+                }
+                list.push(l);
+            }
+        }
+        // [[2,2],[1,1]]
+        // console.log('list = ' + JSON.stringify(list));
+        list.sort((a,b)=>{
+            return a[0].value - b[0].value;
+        });
+        console.log('list = ' + JSON.stringify(list));
+        let groupList = [];
+        let length = Math.round(cardsA.length * 0.5);
+        console.log('length  = ' + length);
+        for (let i = 0 ; i < (list.length - length + 1); i ++){
+            let l = [];
+            for (let j = i ; j < (i + length) ; j ++){
+                l.push(list[j]);
+            }
+            groupList.push(l);
+        }
+        console.log('group list = ' + JSON.stringify(groupList));
+        let doubleScrollList = [];
+        for (let i = 0 ; i < groupList.length ; i ++){
+            let group = groupList[i];
+            console.log('group = ' + JSON.stringify(group));
+            let flag = true;
+            for (let j = 0 ; j < (group.length - 1) ;  j ++){
+                let cards = group[j];
+                console.log('cards = ' + JSON.stringify(cards));
+                if (Math.abs(group[j][0].value - group[j + 1][0].value) !== 1){
+                    flag = false;
+                }
+            }
+            console.log('flag  = ' + flag);
+            if (flag === true){
+                let endList = [];
+                for (let j = 0 ; j < group.length ; j ++){
+                    endList.push(group[j][0]);
+                    endList.push(group[j][1]);
+
+                }
+                let valueB = getDoubleScorllMinValue(endList);
+                if (valueB > valueA){
+                    doubleScrollList.push(endList);
+                }
+            }
+        }
+        let kingBoom = getKingBoom(cardsB);
+        if (kingBoom !== false) {
+            doubleScrollList.push(kingBoom);
+        }
+        let fourBoomList = getFourBoom(cardsB);
+        if (fourBoomList !== false) {
+            // cardsList.push(fourBoom);
+            for (let i = 0; i < fourBoomList.length; i++) {
+                doubleScrollList.push(fourBoomList[i]);
+            }
+        }
+        return doubleScrollList;
     };
 
     // 获得出牌提示
